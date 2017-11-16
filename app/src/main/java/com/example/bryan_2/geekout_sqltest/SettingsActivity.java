ffa56c276328ea41142d1aa65cf84cde65f072bb
Kevin Schechter <kschechter@rocketmail.com>
2017-11-15 23:08:40 -0500

    incomplete settings implementation

---
+++ app/src/main/java/com/example/bryan_2/geekout_sqltest/SettingsActivity.java
@@ -0,0 +1,33 @@
+package com.example.bryan_2.geekout_sqltest;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by kschechter on 11/15/2017.
+ */
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+        SharedPreferences settingsPrefs = getSharedPreferences
+                (MainActivity.SETTINGS_PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor settingsPrefsEditor = settingsPrefs.edit();
+
+        // Settings have not been initialized yet, should not happen here
+        if (settingsPrefs.getAll().isEmpty()) {
+            settingsPrefsEditor.putInt(MainActivity.GAME_MODE, MainActivity.POINTS_MODE);
+            settingsPrefsEditor.putInt(MainActivity.MAX_POINTS, MainActivity.DEFAULT_POINTS);
+            settingsPrefsEditor.putInt(MainActivity.MAX_MINUTES, MainActivity.DEFAULT_MINUTES);
+            settingsPrefsEditor.putInt(MainActivity.MAX_ROUNDS, MainActivity.DEFAULT_ROUNDS);
+        } else {
+
+        }
+
+        settingsPrefsEditor.commit();
+    }
+}
