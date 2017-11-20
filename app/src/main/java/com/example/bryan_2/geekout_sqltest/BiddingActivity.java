b640756e4b9f6f8a3e6dfae5bf6704b5092872b9
cmcnulty <cmcnulty@terpmail.umd.edu>
2017-11-19 19:55:43 -0500

    The basic pieces of bidding and naming

---
+++ app/src/main/java/com/example/bryan_2/geekout_sqltest/BiddingActivity.java
@@ -0,0 +1,209 @@
+package com.example.bryan_2.geekout_sqltest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by colin on 11/19/17.
+ */
+
+
+public class BiddingActivity extends Activity {
+    public static final String QUESTION_KEY = "QuestionString";
+    public static final String NAMING_TEAM_KEY = "NamingTeamString";
+    public static final String TARGET_KEY = "TargetString";
+
+    public static final int BEGIN_NAMING_REQUEST = 12;
+
+    Button bidButton;
+    Button passButton;
+    ImageButton incrementButton;
+    ImageButton decrementButton;
+    TextView teamName;
+    TextView currentBidView;
+    TextView questionView;
+
+    // NOTE: This code is assuming that team names are unique
+    HashMap<String, Integer> score;
+    List<String> allTeams;
+    List<String> biddingTeams = new ArrayList<String>(); //< All teams currently bidding.  As teams pass, names will be removed
+    String leadingTeam; //< The team with the current accepted bid
+    String biddingTeam;
+    String question;
+    int previousBid;
+    int minimumBid;
+    int currentBid;
+    int firstBidder = 0; //< Increments every round
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bidding);
+
+        bidButton = findViewById(R.id.bidButton);
+        passButton = findViewById(R.id.passButton);
+        incrementButton = findViewById(R.id.incrementBidButton);
+        incrementButton.setImageResource(android.R.drawable.ic_input_add); // TODO figure out why this isn't working from xml
+        decrementButton = findViewById(R.id.decrementBidButton);
+        decrementButton.setImageResource(android.R.drawable.ic_input_delete); // TODO ditto
+        teamName = findViewById(R.id.teamNameView);
+        currentBidView = findViewById(R.id.currentBidView);
+
+        // TODO get the list of teams, current question, question minimum bet... somewhere
+        // Until we implement a way to pass that stuff around, use hardcoded debug values
+        allTeams = new ArrayList<String>();
+        allTeams.add("Team 1");
+        allTeams.add("Team 2");
+        allTeams.add("Team 3");
+
+        // Everyone starts with a score of 0
+        score = new HashMap<String, Integer>();
+        for (int i = 0; i < allTeams.size(); i++)
+        {
+            score.put(allTeams.get(i), 0);
+        }
+
+        questionView = findViewById(R.id.biddingQuestionView);
+
+        startNewRound();
+
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                leadingTeam = biddingTeam;
+                previousBid = currentBid;
+                // Next team needs to bet at least one more
+                currentBid++;
+                biddingTeam = getNextTeam();
+
+                // TODO "pass phone" pop-up
+
+                updateViews();
+            }
+        });
+
+        passButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // This was the last team to pass.  Current leader wins the bid
+                if (biddingTeams.size() == 2)
+                {
+                    // TODO "pass the phone" pop-up
+
+                    Intent namingIntent = new Intent(BiddingActivity.this, NamingActivity.class);
+                    namingIntent.putExtra(QUESTION_KEY, question);
+                    namingIntent.putExtra(NAMING_TEAM_KEY, leadingTeam);
+                    namingIntent.putExtra(TARGET_KEY, previousBid);
+                    startActivityForResult(namingIntent, BEGIN_NAMING_REQUEST);
+                    return;
+                }
+                currentBid = previousBid + 1;
+                String nextTeam = getNextTeam();
+                biddingTeams.remove(biddingTeam); // This team passed, so they're no longer part of the bidding loop
+                biddingTeam = nextTeam;
+
+                // TODO "pass the phone" pop-up
+
+                updateViews();
+            }
+        });
+
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentBid++;
+                updateViews();
+            }
+        });
+
+        decrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentBid--;
+
+                // Can't bid less than the minimum for the round
+                if (currentBid < minimumBid)
+                    currentBid = minimumBid;
+                // Can't bid less that the previous team
+                else if (currentBid <= previousBid && previousBid != minimumBid)
+                    currentBid = previousBid+1;
+
+                updateViews();
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        // If we don't recognize the request code, do nothing
+        if (requestCode == BEGIN_NAMING_REQUEST)
+        {
+            if (resultCode > 0)
+            {
+                // Leading team shouldn't change between starting NamingActivity and getting its resutls
+                score.put(leadingTeam, score.get(leadingTeam) + 1);
+            }
+
+            else if (resultCode < 0)
+            {
+                // The score is allowed to go negative.  This is not a bug.
+                // Leading team shouldn't change between starting NamingActivity and getting its resutls
+                score.put(leadingTeam, score.get(leadingTeam) - 2);
+            }
+
+            startNewRound();
+        }
+    }
+
+    void startNewRound()
+    {
+        minimumBid = 1;
+        previousBid = minimumBid;
+        currentBid = minimumBid;
+
+        biddingTeams.clear();
+        biddingTeams.addAll(allTeams);
+
+        biddingTeam = biddingTeams.get(firstBidder++);
+        leadingTeam = biddingTeam; // To prevent the case where everyone passes without bidding, the first team has to bet at least the minimum
+
+        // Don't bother holding on to this view.  We need to set it once and never interact again
+        question = "Some default question.";
+        questionView.setText(question);
+
+
+        updateViews();
+    }
+
+    void updateViews()
+    {
+        teamName.setText(biddingTeam);
+        currentBidView.setText(Integer.toString(currentBid));
+    }
+
+    String getNextTeam()
+    {
+        int index = biddingTeams.indexOf(biddingTeam) + 1;
+
+        // We've wrapped around to the beginning of the list
+        if (index >= biddingTeams.size())
+            return biddingTeams.get(0);
+
+        return biddingTeams.get(index);
+    }
+}
---
+++ app/src/main/java/com/example/bryan_2/geekout_sqltest/NamingActivity.java
@@ -0,0 +1,112 @@
+package com.example.bryan_2.geekout_sqltest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by colin on 11/19/17.
+ */
+
+public class NamingActivity extends Activity {
+    String timerText;
+
+    TextView questionView;
+    TextView teamNameView;
+    TextView scoreView;
+    TextView timerView;
+    ImageButton addButton;
+    ImageButton subtractButton;
+    CountDownTimer timer;
+
+    String question;
+    String namingTeam;
+    int goal;
+    int currentScore;
+    int timeLimit; // in seconds
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent callingIntent = getIntent();
+        question = callingIntent.getStringExtra(BiddingActivity.QUESTION_KEY);
+        namingTeam = callingIntent.getStringExtra(BiddingActivity.NAMING_TEAM_KEY);
+        goal = callingIntent.getIntExtra(BiddingActivity.TARGET_KEY, -1);
+        currentScore = 0;
+
+        if (question == null || namingTeam == null || goal == -1)
+        {
+            //TODO what to do on error like this?
+            return;
+        }
+
+        timeLimit = 5; // TODO
+        timerText = getString(R.string.timeRemaining);
+
+        setContentView(R.layout.activity_naming);
+        questionView = findViewById(R.id.namingQuestionView);
+        questionView.setText(question);
+
+        teamNameView = findViewById(R.id.namingTeamView);
+        teamNameView.setText(namingTeam);
+
+        scoreView = findViewById(R.id.currentScoreView);
+        scoreView.setText(Integer.toString(currentScore));
+
+        timerView = findViewById(R.id.namingTimerView);
+
+        addButton = findViewById(R.id.incrementScoreButton);
+        subtractButton = findViewById(R.id.decrementScoreButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentScore++;
+
+                if (currentScore >= goal)
+                {
+                    // TODO go to victory
+                    timer.cancel();
+                    setResult(1);
+                    finish();
+                }
+                scoreView.setText(Integer.toString(currentScore));
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentScore--;
+                if (currentScore <= 0)
+                    currentScore =0;
+
+                scoreView.setText(Integer.toString(currentScore));
+            }
+        });
+
+        timer = new CountDownTimer(timeLimit * 1000, 1000) {
+            @Override
+            public void onTick(long l) {
+                timerView.setText(timerText + l/1000 + " seconds");
+            }
+
+            @Override
+            public void onFinish() {
+                //TODO go to failure
+                setResult(-1);
+                finish();
+            }
+        };
+
+        timer.start();
+    }
+}
